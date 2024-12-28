f52562aee93a4eb1b6d3b16bc21605039e55f613
Pongchai Hantapong <pongchai.han@student.mahidol.edu>
2024-12-28 10:36:37 +0700

    Add Demo demonstrates some functionality

--- src/demo.py
+++ src/demo.py
@@ -1,25 +1,33 @@
 from src.aggregator import AggregatorClient
+from src.binance import BinanceClient
 from src.utils.logger import get_logger
 
 logger = get_logger(__name__)
 
 
-def place_order_best_price():
+def place_order_best_price(symbol="BTCUSDT", side="buy", quantity="0.5"):
     client = AggregatorClient()
 
     # Fetch the best price
     try:
-        best_info = client.get_best_price(symbol="BTCUSDT", side="buy")
+        best_info = client.get_best_price(symbol=symbol, side=side)
         best_exchange = best_info["exchange"]
         best_price = best_info["price"]
         logger.info(f"Best price is {best_price} on {best_exchange.__class__.__name__}")
 
         # Place order note: if `buy` quantity should be USDT to spend, if `sell` quantity should be BTC to sell
-        response = client.place_order("buy", "15", "BTCUSDT")
+        response = client.place_order(side, quantity, symbol)
         logger.info(f"Order placed: {response}")
     except Exception as e:
         logger.error(f"Failed to place order: {e}")
         return
+
+
+def get_balance():
+    client = BinanceClient()
+    balance = client.get_balance()
+    logger.info(f"Balance: {balance}")
+    return balance
 
 
 if __name__ == "__main__":
